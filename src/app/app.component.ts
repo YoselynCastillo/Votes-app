a55cc7aee0aa4841c278d2c4d67af3aa12ee6935
YoselynCastillo <yose.castillo06@gmail.com>
2020-06-08 19:42:39 -0400

    Se añadio componente link. Añade enlaces, limpia campos. Esta lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Link } from './link/link.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  links: Link[];
 
-  addLink(title:HTMLInputElement, link:HTMLInputElement) {
-    console.log('Values: ', title.value, link.value);
+  constructor() {
+    this.links = [
+      new Link('Angular', 'https://angular.io/cli', 100),
+      new Link('Google', 'http://google.com/', 150),
+      new Link('Youtube', 'https://www.youtube.com/', 200),
+    ];
+    console.log(this.links);
+  }
+  addLink(title: HTMLInputElement, link: HTMLInputElement) {
+    this.links.push(new Link(title.value, link.value));
+    title.value = '';
+    link.value = '';
     return false;
   }
 }
